8363deb92799a8d4238aef0711ab33a57e0377f9
Joel <michaelbluth231@gmail.com>
2023-05-28 13:15:38 +0530

    updated page for feedback"

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   // console.log(params.slug);
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
-      Thank you for your feedback
+      Thank you for your feedback I will definitly look into it
     </div>
   );
 }
